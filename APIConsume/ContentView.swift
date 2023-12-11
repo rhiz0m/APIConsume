c80814a9baf43f05aee56a581673ddded8c37821
Andreas Antonsson <antonsson.andreas@hotmail.com>
2023-12-11 17:16:27 +0100

    first files

--- APIConsume/ContentView.swift
+++ APIConsume/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var apiResponse: String = "Loading..."
+    private let baseURL = "http://localhost:3000/"
+    private let testEndpoint = "test"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(apiResponse)
+            .onAppear {
+                fetchData()
+            }
+    }
+
+    func fetchData() {
+        
+        // Make a GET request to Express API
+        if let url = URL(string: baseURL+testEndpoint) {
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    if let apiResponse = String(data: data, encoding: .utf8) {
+                        // Update the UI on the main thread
+                        DispatchQueue.main.async {
+                            self.apiResponse = apiResponse
+                        }
+                    }
+                }
+            }.resume()
         }
-        .padding()
     }
 }
 
