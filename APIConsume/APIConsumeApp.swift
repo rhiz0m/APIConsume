7e7be168a6675f82737d4e02632198bb266a7863
Andreas Antonsson <antonsson.andreas@hotmail.com>
2023-12-11 16:13:25 +0100

    Initial Commit

---
+++ APIConsume/APIConsumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIConsumeApp.swift
+//  APIConsume
+//
+//  Created by Andreas Antonsson on 2023-12-11.
+//
+
+import SwiftUI
+
+@main
+struct APIConsumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIConsume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  APIConsume
+//
+//  Created by Andreas Antonsson on 2023-12-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
